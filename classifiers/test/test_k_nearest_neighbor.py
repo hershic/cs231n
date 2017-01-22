054f51bee78ab252795546322d4f321d1dfa8d6d
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-21 20:58:12 -0600

    appease linter more

--- classifiers/test/test_k_nearest_neighbor.py
+++ classifiers/test/test_k_nearest_neighbor.py
@@ -1,10 +1,10 @@
 import unittest
-import random
 import numpy as np
 from util.data import load_CIFAR10
 from classifiers import KNearestNeighbor
 
 cifar10_dir = 'datasets/cifar-10-batches-py'
+
 
 class TestKNearestNeighbor(unittest.TestCase):
   def setUp(self):
@@ -23,7 +23,6 @@ class TestKNearestNeighbor(unittest.TestCase):
     self.classifier = KNearestNeighbor()
     self.classifier.train(self.train_points, self.train_labels)
 
-
   def subsample(self, train_points, train_labels, test_points, test_labels, num_train, num_test):
     mask = range(num_train)
     train_points = train_points[mask]
@@ -35,12 +34,11 @@ class TestKNearestNeighbor(unittest.TestCase):
 
     return (train_points, train_labels, test_points, test_labels)
 
-
   def test_dist_two_loops(self):
     dists_two = self.classifier.compute_distances_two_loops(self.test_points)
 
-    # Now implement the function predict_labels and run the code below: We use k
-    # = 1 (which is Nearest Neighbor).
+    # Now implement the function predict_labels and run the code below: We use
+    # k = 1 (which is Nearest Neighbor).
     test_labels_pred = self.classifier.predict_labels(dists_two, k=5)
 
     # Compute and print the fraction of correctly predicted examples
@@ -53,7 +51,6 @@ class TestKNearestNeighbor(unittest.TestCase):
     self.assertLess(accuracy, 0.3)
     self.assertGreater(accuracy, 0.1)
 
-
   def test_dist_one_loop(self):
     # Now lets speed up distance matrix computation by using partial
     # vectorization with one loop. Implement the function
@@ -61,19 +58,19 @@ class TestKNearestNeighbor(unittest.TestCase):
     dists_two = self.classifier.compute_distances_two_loops(self.test_points)
     dists_one = self.classifier.compute_distances_one_loop(self.test_points)
 
-    # To ensure that our vectorized implementation is correct, we make sure that it
-    # agrees with the naive implementation. There are many ways to decide whether
-    # two matrices are similar; one of the simplest is the Frobenius norm. In case
-    # you haven't seen it before, the Frobenius norm of two matrices is the square
-    # root of the squared sum of differences of all elements; in other words, reshape
-    # the matrices into vectors and compute the Euclidean distance between them.
+    # To ensure that our vectorized implementation is correct, we make sure
+    # that it agrees with the naive implementation. There are many ways to
+    # decide whether two matrices are similar; one of the simplest is the
+    # Frobenius norm. In case you haven't seen it before, the Frobenius norm of
+    # two matrices is the square root of the squared sum of differences of all
+    # elements; in other words, reshape the matrices into vectors and compute
+    # the Euclidean distance between them.
     difference = np.linalg.norm(dists_two - dists_one, ord='fro')
     self.assertAlmostEqual(difference, 0.0)
 
-
   def test_dist_no_loops(self):
-    # Now implement the fully vectorized version inside compute_distances_no_loops
-    # and run the code
+    # Now implement the fully vectorized version inside
+    # compute_distances_no_loops and run the code
     dists_one = self.classifier.compute_distances_one_loop(self.test_points)
     dists_none = self.classifier.compute_distances_no_loops(self.test_points)
 
@@ -81,19 +78,17 @@ class TestKNearestNeighbor(unittest.TestCase):
     difference = np.linalg.norm(dists_one - dists_none, ord='fro')
     self.assertAlmostEqual(difference, 0.0)
 
-
   # Auxiliary method to time function calls
   def time_function(self, f, *args):
     """
-    Call a function f with args and return the time (in seconds) that it took to
-    execute.
+    Call a function f with args and return the time (in seconds) that it took
+    to execute.
     """
     import time
     tic = time.time()
     f(*args)
     toc = time.time()
     return toc - tic
-
 
   # Ensure the algorithms are sane
   def test_timing(self):
@@ -105,4 +100,4 @@ class TestKNearestNeighbor(unittest.TestCase):
     # about 10x faster than the 1-loop version; the 1-loop version should be of
     # similar speed (but slightly faster) compared to the 2-loop version
     self.assertLess(one_loop_time, two_loop_time)
-    self.assertLess(no_loop_time*10, one_loop_time)
+    self.assertLess(no_loop_time * 10, one_loop_time)
