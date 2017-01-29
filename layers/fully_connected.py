33d6678f1aafb670935702f4db56a89d463ce61d
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-29 16:24:54 -0600

    add LayerFullyConnected with naive forward pass and test

---
+++ layers/fully_connected.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class LayerFullyConnected:
+    """
+    Models a fully connected Neural Network layer where each input point is
+    connected to each output score with some weight.
+    """
+    def __init__(self, weights_shape, points_shape, gradientInit=None):
+        num_classifications = weights_shape[0]
+        num_points = points_shape[0]
+
+        # sane defaults
+        if (not gradientInit):
+            self.gradient = np.random.randn(num_classifications, num_points) * 1e-4
+        else:
+            self.gradient = gradientInit
+
+    def forward_naive(self, weights, points):
+        """
+        Computes the forward pass of a fully connected layer with the input
+        weights, returning the category scores for the input points.
+
+        Inputs:
+        - weights: (num_classifications, point_size)
+        - points: (num_points, point_size)
+        Outputs:
+        - scores: (num_points, num_classifications)
+        """
+        num_classifications = weights.shape[0]
+        num_points = points.shape[0]
+        scores = np.zeros((num_points, num_classifications))
+        for i in range(num_points):
+            scores[i] = weights.dot(points[i])
+        return scores
---
+++ layers/test/test_fully_connected.py
@@ -0,0 +1,45 @@
+import unittest
+import numpy as np
+
+from layers.fully_connected import LayerFullyConnected
+
+
+class TestLayerFullyConnected(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_naive_randomized(self):
+        num_classes = 3
+        num_points = 5
+        point_size = 4
+        weights = np.random.randn(num_classes, point_size)
+        points = np.random.randn(num_points, point_size)
+        layer = LayerFullyConnected(weights.shape, points.shape)
+        scores = layer.forward_naive(weights, points)
+
+
+class TestLayerFullyConnectedDirected(unittest.TestCase):
+    def setUp(self):
+        self.weights = np.array([[1, 2, 3, 2],
+                                 [2, 4, 2, 3],
+                                 [3, 1, 2, 4]])
+        self.points = np.array([[1, 3, 2, 1],
+                                [3, 3, 1, 4],
+                                [1, 2, 3, 1],
+                                [2, 3, 2, 2],
+                                [2, 3, 1, 3]])
+        self.expected = np.array([[15, 21, 14],
+                                  [20, 32, 30],
+                                  [16, 19, 15],
+                                  [18, 26, 21],
+                                  [17, 27, 23]])
+
+    def test_naive_directed(self):
+        layer = LayerFullyConnected(self.weights.shape, self.points.shape)
+        scores = layer.forward_naive(self.weights, self.points)
+        self.assertTrue(np.array_equal(scores, self.expected))
+
+    def test_vectorized_directed(self):
+        layer = LayerFullyConnected(self.weights.shape, self.points.shape)
+        scores = layer.forward_naive(self.weights, self.points)
+        self.assertTrue(np.array_equal(scores, self.expected))
