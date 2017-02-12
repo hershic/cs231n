8cff932669ad89429508a56b9456fbe6cda57051
Eric Crosson <esc@ericcrosson.com>
2017-02-12 14:30:07 -0600

    Operate in the `datasets` directory

--- datasets/get_datasets.sh
+++ datasets/get_datasets.sh
@@ -1,4 +1,29 @@
-# Get CIFAR10
-wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xzvf cifar-10-python.tar.gz
-rm cifar-10-python.tar.gz 
+#!/usr/bin/env bash
+#
+# This script fetches CIFAR10 dataset.
+
+set -o nounset
+
+readonly cifar10="cifar-10-python.tar.gz"
+
+# Operate in the `/datasets` directory
+cd $(readlink -f $(dirname $0))
+
+# Download cifar10
+init() {
+    trap teardown EXIT
+    wget http://www.cs.toronto.edu/~kriz/${cifar10}
+}
+
+# Remove archived cifar10
+teardown() {
+    rm -f ${cifar10}
+}
+
+# Extract cifar10
+main() {
+    tar -xzf ${cifar10}
+}
+
+init
+main
