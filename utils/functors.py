183e6cda68df9b4c7d074fd188d9e326659c655f
Hershal Bhave <hershal.bhave@gmail.com>
2017-02-26 12:07:00 -0600

    Clean up documentation

--- utils/functors.py
+++ utils/functors.py
@@ -21,7 +21,7 @@ def compose(*functions):
 
     Example:
     - y = compose(f, g, h) returns a function which applies f(g(h(x))). We can
-      call the reduction simply by issuing y(x), given a compatible input x.
+      call the reduction simply by calling y(x), given a compatible input x.
     """
     def _composed(f, g):
         return lambda x: f(g(x))
@@ -32,9 +32,9 @@ def compose(*functions):
 def chain(*functions):
     """
     Creates a function chain (i.e. h(g(f(x))) given (f, g, h)) of the given
-    functions, in which the each list element is applied, in turn, to the
-    input, chaining the output from the previous function application to the
-    next function application.
+    functions, in which the each element is applied, in turn, to the input,
+    chaining the output from the previous function to the input of the next
+    function.
 
     Inputs:
     - *functions: The functions to chain.
@@ -46,11 +46,11 @@ def chain(*functions):
       - The input to the chain, e.g. x
 
       Outputs:
-      - The result of the composition, e.g. h(g(f(x)))
+      - The result of the chain, e.g. h(g(f(x)))
 
     Example:
     - y = chain(f, g, h) returns a function which applies h(g(f(x))). We can
-      call the reduction simply by issuing y(x), given a compatible input x.
+      call the reduction simply by calling y(x), given a compatible input x.
     """
     def _chained(f, g):
         return lambda x: g(f(x))
