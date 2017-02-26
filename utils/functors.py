e721c05aea44d7d7e55e95fb385efbe41e8b1bda
Hershal Bhave <hershal.bhave@gmail.com>
2017-02-26 12:09:17 -0600

    Sort functions

--- utils/functors.py
+++ utils/functors.py
@@ -1,34 +1,6 @@
 import functools
 
 
-def compose(*functions):
-    """
-    Creates a function composition (e.g. f(g(h(x))) given (f, g, h)) of the
-    given functions, in which the innermost function is the rightmost list
-    element.
-
-    Inputs:
-    - *functions: The functions to compose.
-
-    Outputs:
-    - reduction(): A function which applies the composition
-
-      Inputs:
-      - The input to the composition, e.g. x
-
-      Outputs:
-      - The result of the composition, e.g. f(g(h(x)))
-
-    Example:
-    - y = compose(f, g, h) returns a function which applies f(g(h(x))). We can
-      call the reduction simply by calling y(x), given a compatible input x.
-    """
-    def _composed(f, g):
-        return lambda x: f(g(x))
-
-    return functools.reduce(_composed, functions, lambda x: x)
-
-
 def chain(*functions):
     """
     Creates a function chain (i.e. h(g(f(x))) given (f, g, h)) of the given
@@ -56,3 +28,31 @@ def chain(*functions):
         return lambda x: g(f(x))
 
     return functools.reduce(_chained, functions, lambda x: x)
+
+
+def compose(*functions):
+    """
+    Creates a function composition (e.g. f(g(h(x))) given (f, g, h)) of the
+    given functions, in which the innermost function is the rightmost list
+    element.
+
+    Inputs:
+    - *functions: The functions to compose.
+
+    Outputs:
+    - reduction(): A function which applies the composition
+
+      Inputs:
+      - The input to the composition, e.g. x
+
+      Outputs:
+      - The result of the composition, e.g. f(g(h(x)))
+
+    Example:
+    - y = compose(f, g, h) returns a function which applies f(g(h(x))). We can
+      call the reduction simply by calling y(x), given a compatible input x.
+    """
+    def _composed(f, g):
+        return lambda x: f(g(x))
+
+    return functools.reduce(_composed, functions, lambda x: x)
